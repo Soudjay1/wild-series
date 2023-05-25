550c4f2ecc7d7806a4f14444e83ee376ee95d69f
Abdulkader <youssouf.soudjay@gmail.com>
2023-05-25 12:55:54 +0200

    Retrieve data stored with Doctrine

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,7 +5,7 @@ use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-#[Route('/category ', name: 'category_')]
+#[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
 {
 #[Route('/', name: 'index')]
@@ -16,4 +16,14 @@ public function index(CategoryRepository $categoryRepository):Response
         'category/index.html.twig',
         ['category' => $category]);
 }
+    #[Route('/{id}', name: 'show')]
+    public function show(int $id, CategoryRepository $categoryRepository):Response
+    {
+        $categories = $categoryRepository->findBy(['id' => $id],['id' => 'DESC'],3);
+
+
+        return $this->render('category/show.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
 }
